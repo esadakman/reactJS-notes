131c9ef5f380398abe96ab80d20b99adb4062734
esadakman <98649983+esadakman@users.noreply.github.com>
2022-06-28 12:33:26 +0300

    session_4_tour_places solution added

---
+++ session_04_tour-places-rp-01/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import './Navbar.scss';
+import React from 'react';
+
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <a href="#aboutus">About Us</a>
+            <a href="#foryou">For You</a>
+            <a href="#services">Services</a>
+            <a href="#blog">Blog</a>
+            <a href="#vlog">Vlog</a>
+            <a href="#contact">Contact</a>
+        </div>
+    )
+}
+
+export default Navbar;
+
